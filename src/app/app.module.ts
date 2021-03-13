9ee182dd4ad33fcd6b2a93e824cd699b3c55a8e0
santanaluc <lucas.s.s.silva@hotmail.com>
2021-03-12 21:25:00 -0300

    Adicionando navbar responsiva

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { NavbarComponent } from "./navbar/navbar.component";
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, NavbarComponent, HomeComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent, NavbarComponent, HomeComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
