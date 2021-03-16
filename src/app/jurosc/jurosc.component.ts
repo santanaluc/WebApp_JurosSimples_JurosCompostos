3643d3fc006d7f64641927561d594b776ffce94b
santanaluc <lucas.s.s.silva@hotmail.com>
2021-03-16 13:57:35 -0300

    2 files modified

--- src/app/jurosc/jurosc.component.ts
+++ src/app/jurosc/jurosc.component.ts
@@ -16,17 +16,20 @@ export class JuroscComponent implements OnInit {
     this.t;
   }
 
-  getJuros() {
-    if (this.c >= 0 && this.i >= 0) {
-      if (this.t >= 0) {
-        let montante: number = this.c * Math.pow(1 + this.i / 100, this.t);
-        let juros: number = montante - this.c;
-        return juros.toFixed(2);
+  getJurosList() {
+    let list: string[] = [];
+
+    for (let j = 1; j <= this.t; j++) {
+      if (this.c >= 0 && this.i >= 0) {
+        if (j >= 0) {
+          let montante: number = this.c * Math.pow(1 + this.i / 100, j);
+          let convert = montante.toFixed(2);
+          list.push(convert);
+        }
       }
     }
+    return list;
   }
-
-  acumular() {}
 
   ngOnInit() {}
 }
