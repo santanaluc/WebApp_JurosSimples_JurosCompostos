7457187ea03db7fac021a66829d62a5dcca771b7
santanaluc <lucas.s.s.silva@hotmail.com>
2021-03-17 14:39:35 -0300

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,22 +7,4 @@ import { Component, VERSION } from "@angular/core";
 })
 export class AppComponent {
   name = "Lucas S. Santana da Silva";
-  timestamp = new Date();
-
-  getHour() {
-    return this.timestamp.getHours();
-  }
-
-  isMorning() {
-    return this.getHour() > 6 && this.getHour() <= 12;
-  }
-  isAfternoon() {
-    return this.getHour() > 12 && this.getHour() <= 18;
-  }
-  isNight() {
-    return this.getHour() > 18 && this.getHour() <= 24;
-  }
-  isDawn() {
-    return this.getHour() <= 6;
-  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  timestamp = new Date();
 
-  ngOnInit() {
+  getHour() {
+    return this.timestamp.getHours();
   }
 
+  isMorning() {
+    return this.getHour() > 6 && this.getHour() <= 12;
+  }
+  isAfternoon() {
+    return this.getHour() > 12 && this.getHour() <= 18;
+  }
+  isNight() {
+    return this.getHour() > 18 && this.getHour() <= 24;
+  }
+  isDawn() {
+    return this.getHour() <= 6;
+  }
+
+  ngOnInit() {}
 }
